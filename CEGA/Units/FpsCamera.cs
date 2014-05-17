af8695d57bb84b8d20dfd7c74bdab3bafed51343
Javier Joaquin <javierous@gmail.com>
2014-05-16 21:36:23 -0300

    Esqueleto de la nueva camara.

---
+++ CEGA/Units/FpsCamera.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.DirectX;
+using TgcViewer.Utils.Input;
+
+namespace AlumnoEjemplos.CEGA.Units
+{
+    /// <summary>
+    /// Controla una camara en primera persona.
+    /// </summary>
+    class FpsCamera : TgcCamera
+    {
+        public Vector3 getPosition()
+        {
+            return new Vector3();
+        }
+
+        public Vector3 getLookAt()
+        {
+            return new Vector3();
+        }
+
+        /// <summary>
+        /// Actualizar el estado interno de la cámara en cada frame
+        /// </summary>
+        public void updateCamera()
+        {
+
+        }
+
+        /// <summary>
+        /// Actualizar la matriz View en base a los valores de la cámara
+        /// </summary>
+        public void updateViewMatrix(Microsoft.DirectX.Direct3D.Device d3dDevice)
+        {
+
+        }
+
+        public bool Enable { get; set; }
+    }
+}
